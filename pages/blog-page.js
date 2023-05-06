216e5bc71887c2b2897231fd85b12482cad0e365
tachoco <vanilla.airi@gmail.com>
2023-05-06 17:57:27 +0900

    2-13 getStaticProps

--- pages/blog-page.js
+++ pages/blog-page.js
@@ -1,9 +1,20 @@
 import Layout from "../components/Layout"
+import Post from "../components/Post"
+import { getAllPostsData } from "../lib/posts"
 
-export default function Blog() {
+export default function Blog({ posts }) {
   return (
     <Layout title="Blog">
-      Blog page
+      <ul>
+        {posts && posts.map((post) => <Post key={post.id} post={post} />)}
+      </ul>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const posts = await getAllPostsData();
+  return {
+    props: { posts }
+  }
 }
